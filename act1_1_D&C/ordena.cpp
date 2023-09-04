9028e573a477272a324a4dace5a24c7e6886b482
Pablo Banzo <pridapablo@gmail.com>
2023-09-03 21:43:17 -0600

    doMergeSort implemented

--- act1_1_D&C/ordena.cpp
+++ act1_1_D&C/ordena.cpp
@@ -14,12 +14,57 @@ Date: 03/09/2023
 using namespace std;
 
 /*
-This function will apply the merge sort algorithm to sort an array of integers
+Clean public interface for the mergeSort algorithm (to comply with the
+activity's instructions)
+
 @param arr: the array to be sorted
 
 @return: the sorted array (in ascending order)
 */
-vector<int> mergeSort(vector<int> &arr);
+vector<int> mergeSort(const vector<int> &arr)
+{
+    // Copy the input array
+    vector<int> sortedArray = arr;
+    // Call the recursive function in place setting the lower and upper bounds
+    doMergeSort(sortedArray, 0, sortedArray.size() - 1);
+    return sortedArray;
+}
+
+/*
+Implementation of the merge sort algorithm, initialized based on the "mergeSort"
+call. Will sort the array in place.
+
+@param arr: the array to be sorted @param lo: the lower index of the array
+@param hi: the higher index of the array
+*/
+void doMergeSort(vector<int> &arr, int lo, int hi)
+{
+    if (lo < hi)
+    {
+        return;
+    }
+
+    // prevent overflow of (hi + lo) / 2
+    int mid = lo + (hi - lo) / 2;
+    //    - The first half will contain the elements from arr[0] to arr[mid]
+    // call mergeSort on the first half (recursive call)
+    doMergeSort(arr, lo, mid);
+    //    - The second half will contain the elements from arr[mid + 1] to arr[n - 1]
+    // call mergeSort on the second half (recursive call)
+    doMergeSort(arr, mid + 1, hi);
+
+    // merge the two halves in place
+    merge(arr, lo, mid, hi);
+}
+
+/*
+Helper function for mergeSort. This function will apply the merge step in place
+
+@param arr: the array to be sorted @param lo: the lower index of the array
+@param mid: the middle index of the array @param hi: the higher index of the
+array
+*/
+void merge(vector<int> &arr, int lo, int mid, int hi);
 
 /*
 This function will apply the quick sort algorithm to sort an array of integers
@@ -35,27 +80,6 @@ fileName: the name of the file to be read (with the extension)
 
 @return: the vector with the data
 */
-vector<int> readData(string fileName);
-// alias for readData (to comply with the activity's instructions)
-vector<int> leeDatos(string fileName) { return readData(fileName); }
-
-/*
-This function will print the data of a vector @param data: the vector to be
-printed
-*/
-void printData(vector<int> data);
-// alias for printData (to comply with the activity's instructions)
-void imprimeDatos(vector<int> data) { printData(data); }
-
-/*
-This function will run the merge sort and quick sort algorithms on the data
-stored in a file. It will print the unsorted data, the data sorted with merge
-sort and the data sorted with quick sort.
-*/
-int main();
-
-// Function implementations:
-
 vector<int> readData(string fileName)
 {
     vector<int> data;
@@ -69,8 +93,7 @@ vector<int> readData(string fileName)
 
     string line;
     int n = 0;
-    // read each line of the file
-    // Line 1: n, the number of elements in the array
+    // read each line of the file Line 1: n, the number of elements in the array
     // Line 2...n: the elements of the array
 
     try
@@ -99,7 +122,13 @@ vector<int> readData(string fileName)
 
     return data;
 }
+// alias for readData (to comply with the activity's instructions)
+vector<int> leeDatos(string fileName) { return readData(fileName); }
 
+/*
+This function will print the data of a vector @param data: the vector to be
+printed
+*/
 void printData(vector<int> data)
 {
     for (int i = 0; i < data.size(); i++)
@@ -108,15 +137,21 @@ void printData(vector<int> data)
     }
     cout << endl;
 }
+// alias for printData (to comply with the activity's instructions)
+void imprimeDatos(vector<int> data) { printData(data); }
 
+/*
+This function will run the merge sort and quick sort algorithms on the data
+stored in a file. It will print the unsorted data, the data sorted with merge
+sort and the data sorted with quick sort.
+*/
 int main()
 {
     vector<int> data, sortedData;
     data = readData("data.txt");
     printData(data); // unsorted data
-    // sortedData = mergeSort(data);x
-    // printData(sortedData); // data sorted with Merge Sort
-    // sortedData = quickSort(data);
-    // printData(sortedData); // data sorted with Quick Sort
+    // sortedData = mergeSort(data); printData(sortedData); // data sorted with
+    // Merge Sort sortedData = quickSort(data); printData(sortedData); // data
+    // sorted with Quick Sort
     return 0;
 }
