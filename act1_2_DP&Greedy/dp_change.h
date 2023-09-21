a8cbb31d378a7306cdfef540ce545a0b78fe0719
Pablo Banzo <pridapablo@gmail.com>
2023-09-21 09:23:31 -0600

    add fn signatures

--- act1_2_DP&Greedy/dp_change.h
+++ act1_2_DP&Greedy/dp_change.h
@@ -2,6 +2,13 @@
 #ifndef DP_CHANGE_H_
 #define DP_CHANGE_H_
 
-// NOT YET IMPLEMENTED
+#include <vector>
+
+using namespace std;
+
+vector<int> dpChange(int change, vector<int> coinTypes)
+{
+    // IMPLEMENT
+}
 
 #endif // DP_CHANGE_H_
--- act1_2_DP&Greedy/greedy_change.h
+++ act1_2_DP&Greedy/greedy_change.h
@@ -2,6 +2,13 @@
 #ifndef GREEDY_CHANGE_H_
 #define GREEDY_CHANGE_H_
 
-// NOT YET IMPLEMENTED
+#include <vector>
+
+using namespace std;
+
+vector<int> greedyChange(int change, vector<int> coinTypes)
+{
+    // IMPLEMENT
+}
 
 #endif // GREEDY_CHANGE_H_
