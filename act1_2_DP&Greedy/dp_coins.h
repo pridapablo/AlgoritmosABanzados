74a6ac16490f553e716f07367f38c2ea745a77df
Pablo Banzo <pridapablo@gmail.com>
2023-09-21 09:16:41 -0600

    add initial structure for act1_2

---
+++ act1_2_DP&Greedy/dp_coins.h
@@ -0,0 +1,7 @@
+// Dynamic programming version of coin change problem.
+#ifndef DP_COINS_H_
+#define DP_COINS_H_
+
+// NOT YET IMPLEMENTED
+
+#endif // DP_COINS_H_
---
+++ act1_2_DP&Greedy/greedy_coins.h
@@ -0,0 +1,7 @@
+// Greedy algorithm for coin change problem
+#ifndef GREEDY_COINS_H_
+#define GREEDY_COINS_H_
+
+// NOT YET IMPLEMENTED
+
+#endif // GREEDY_COINS_H_
