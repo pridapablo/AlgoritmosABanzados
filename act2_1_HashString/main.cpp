c6bf7fa19b0327f0d8e0ea25820744dfa8cefd86
Pablo Banzo <pridapablo@gmail.com>
2023-10-14 11:43:20 -0600

    added time complexity

--- act2_1_HashString/main.cpp
+++ act2_1_HashString/main.cpp
@@ -9,11 +9,13 @@
 #include <iostream>
 #include <string>
 #include <fstream>
+#include <vector>
 
 using namespace std;
 
 /*
-Function to read a file and return its content as a string
+Function to read a file and return its content as a string.
+O(m) time complexity (where m is the length of the content of the file).
 
 @param fileName: the name of the file to read
 @return content: the content of the file
@@ -34,13 +36,13 @@ string readFiles(string fileName)
     }
     else
     {
-        cout << "Unable to open file";
         return "";
     }
 }
 
 /*
-Function to validate the value of n (the number of columns in the table where the characters will be placed including line breaks)
+Function to validate the value of n (the number of columns in the table where the characters will be placed including line breaks).
+O(1) time complexity (constant).
 
 @param n: the value of n (should be a multiple of 4 and between 16 and 64)
 @return true if n is valid, false otherwise
@@ -58,33 +60,34 @@ bool validateN(int n)
 }
 
 /*
-Function to convert a number to its hexadecimal representation
+Function to convert a number to its hexadecimal representation.
+Loops until the number is 0 but the maximum input is 255, so the time complexity is O(1) (constant).
 
 @param n: the number to convert
 @return hex: the hexadecimal representation of the number
 */
 string toHex(int n)
 {
-    string hex; // the function returns this string (the hexadecimal representation of the number)
-    int remainder;
-    while (n != 0)
+    string hex = "";
+    while (n)
     {
-        remainder = n % 16;
-        if (remainder < 10)
-        {
-            hex = char(remainder + 48) + hex;
-        }
+        int temp = n % 16;
+        if (temp < 10)
+            hex += temp + 48;
         else
-        {
-            hex = char(remainder + 55) + hex;
-        }
+            hex += temp + 55;
         n /= 16;
+    }
+    while (hex.length() < 2) // Ensure it's two characters
+    {
+        hex = "0" + hex;
     }
     return hex;
 }
 
 /*
-Function to hash the content of the file
+Function to hash the content of the file. Since n is bounded by 64 (constant), the time complexity of this function is
+only related to the length of the content of the file, which is O(m), where m is the length of the content of the file.
 
 @param content: the content of the file
 @param n: the value of n (the number of columns in the table where the characters will be placed including line breaks)
@@ -117,10 +120,10 @@ string hashString(string content, int n)
     }
 
     // Generate the hash string
-    for (int i = 0; i < n; i += 4)
+    for (int i = 0; i < n; i += 4) // Looping every 4 columns to generate n/4 hex characters
     {
-        int value = asciiTable[i] + asciiTable[i + 1] + asciiTable[i + 2] + asciiTable[i + 3];
-        hash += toHex(value);
+        string hexValue = toHex(asciiTable[i]); // No need to sum across columns, just mod 256
+        hash += hexValue;
     }
 
     return hash;
@@ -130,25 +133,44 @@ int main()
 {
     string fileName;
     int n;
+    bool isFileRead = false; // flag to check if file has been successfully read
+    bool isNValid = false;   // flag to check if n is valid
 
-    cout << "Ingresa el nombre del archivo: ";
-    cin >> fileName;
+    string content;
 
-    // Read and validate n
-    cout << "Ingresa el valor de n: ";
-    cin >> n;
-    if (validateN(n))
+    while (!isFileRead || !isNValid) // loop until both file is successfully read and n is valid
     {
-        cout << "El valor de n es valido" << endl;
-    }
-    else
-    {
-        cout << "El valor de n no es valido" << endl;
-        return 0;
-    }
+        if (!isFileRead)
+        {
+            cout << "Ingresa el nombre del archivo: ";
+            cin >> fileName;
+            content = readFiles(fileName);
 
-    // Read and extract content from file
-    string content = readFiles(fileName);
+            if (content == "")
+            {
+                cout << "Archivo no encontrado o no se pudo abrir. Intente de nuevo.\n";
+            }
+            else
+            {
+                isFileRead = true;
+            }
+        }
+
+        if (isFileRead && !isNValid)
+        {
+            cout << "Ingresa el valor de n: ";
+            cin >> n;
+            if (validateN(n))
+            {
+                cout << "El valor de n es valido" << endl;
+                isNValid = true;
+            }
+            else
+            {
+                cout << "El valor de n no es valido. Intente de nuevo.\n";
+            }
+        }
+    }
 
     // Hashing
     string hash = hashString(content, n);
