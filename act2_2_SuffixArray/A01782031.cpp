74ef1ca08107732457ccbb1c9ef9e3dbc7e4f1b4
Pablo Banzo <pridapablo@gmail.com>
2023-10-16 23:37:39 -0600

    add complexity and sort

--- act2_2_SuffixArray/A01782031.cpp
+++ act2_2_SuffixArray/A01782031.cpp
@@ -26,7 +26,23 @@ struct Suffix
 };
 
 /*
-Function that calculates the suffix array of a string and returns it.
+Function that compares two suffixes and returns true if the first one is smaller than the second one.
+Time complexity: O(n) where n is the length of longest of the two input suffixes.
+
+@param a: first suffix.
+@param b: second suffix.
+@return: true if a < b, false otherwise.
+*/
+bool compare(const Suffix &a, const Suffix &b)
+{
+    return a.suffix < b.suffix;
+}
+
+/*
+Function that calculates the suffix array of a string and returns it after sorting it lexicographically.
+
+The sorting algorithm implemented in c++ will sort the suffixes lexicographically in O(nlogn) time while
+the for loop will take O(n^2) time. Therefore, the time complexity of this function is O(n^2logn).
 
 @param s: string of length n.
 @return: the suffix array of s in the form of a Suffix struct vector.
@@ -39,9 +55,12 @@ vector<Suffix> suffixArray(const string &s)
     // Fill the vector with the suffixes of the string.
     for (int i = 0; i < n; i++)
     {
-        string suffix = s.substr(i);
-        suffixes.push_back(Suffix(i, suffix)); 
+        string suffix = s.substr(i);           // Get the suffix of the current index.
+        suffixes.push_back(Suffix(i, suffix)); // Add the suffix to the vector.
     }
+
+    sort(suffixes.begin(), suffixes.end(), compare); // Sort the suffixes lexicographically.
+
     return suffixes;
 }
 
@@ -57,8 +76,8 @@ int main()
     cout << "Suffix Array:" << endl;
     for (int i = 0; i < sa.size(); i++)
     {
-        cout << sa[i].index << " ";
-        cout << sa[i].suffix << endl;
+        cout << sa[i].index << " ";   // index
+        cout << sa[i].suffix << endl; // string (suffix)
     }
     cout << endl;
 
